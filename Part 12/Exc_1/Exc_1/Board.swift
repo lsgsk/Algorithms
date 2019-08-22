7981c97c3d710579f21a883eb5dce63db252bf35
lsgsk <lsgsk@rambler.ru>
2019-08-22 18:44:15 +0300

    refact 1 exc from 12 part

--- Part 12/Exc_1/Exc_1/Board.swift
+++ Part 12/Exc_1/Exc_1/Board.swift
@@ -1,9 +1,11 @@
 import Foundation
 
-let boardSideCount = 3
+struct Constants {
+	static let boardSideCount = 3
+}
 
 struct Move {
-	let move: Int
+	let position: Int
 }
 
 enum PlayerType {
@@ -13,7 +15,7 @@ enum PlayerType {
 
 enum GameResult {
 	case unknown
-	case wine(PlayerType)
+	case win(PlayerType)
 	case draw
 }
 
@@ -22,36 +24,27 @@ class Player {
 	private(set) var moves: [Bool]
 	init(type: PlayerType) {
 		self.type = type
-		self.moves = [Bool](repeating: false, count: boardSideCount * boardSideCount)
+		self.moves = [Bool](repeating: false, count: Constants.boardSideCount * Constants.boardSideCount)
 	}
 	
 	func addMove(_ move: Move) {
-		moves[move.move] = true
+		moves[move.position] = true
 	}
 	func removeMove(_ move: Move) {
-		moves[move.move] = false
+		moves[move.position] = false
 	}
 }
 
 class Board {
-	
-	private func genarateBoardState(player1: Player, player2: Player) -> [PlayerType?] {
-		var positions = [PlayerType?](repeating: nil, count: boardSideCount * boardSideCount)
-		setPlayerMoves(board: &positions, player: player1)
-		setPlayerMoves(board: &positions, player: player2)
-		return positions
+	let player1: Player
+	let player2: Player
+	init(player1: Player, player2: Player) {
+		self.player1 = player1
+		self.player2 = player2
 	}
 	
-	private func setPlayerMoves(board: inout [PlayerType?], player: Player) {
-		for (index, item) in player.moves.enumerated() {
-			if item {
-				board[index] = player.type
-			}
-		}
-	}
-
-	func getPossibleMoves(player1: Player, player2: Player) -> [Int] {
-		let positions = genarateBoardState(player1: player1, player2: player2)
+	func getAvailableMoves() -> [Int] {
+		let positions = genarateBoardState()
 		var array = [Int]()
 		for (index, item) in positions.enumerated() {
 			if item == nil {
@@ -61,50 +54,13 @@ class Board {
 		return array
 	}
 	
-	func calculateGameResult(player1: Player, player2: Player) -> GameResult {
-		let positions = genarateBoardState(player1: player1, player2: player2)
+	func calculateGameResult() -> GameResult {
+		let positions = genarateBoardState()
 		return checkGameResult(positions)
 	}
 	
-	func checkGameResult(_ board: [PlayerType?]) -> GameResult {
-		if board[0] != nil &&  board[0] == board[1] && board[1] == board[2] {
-			return GameResult.wine(board[0]!)
-		}
-		if board[3] != nil &&  board[3] == board[4] && board[4] == board[5] {
-			return GameResult.wine(board[3]!)
-		}
-		if board[6] != nil &&  board[6] == board[7] && board[7] == board[8] {
-			return GameResult.wine(board[6]!)
-		}
-		
-		if board[0] != nil &&  board[0] == board[3] && board[3] == board[6] {
-			return GameResult.wine(board[0]!)
-		}
-		if board[1] != nil &&  board[1] == board[4] && board[4] == board[7] {
-			return GameResult.wine(board[1]!)
-		}
-		if board[2] != nil &&  board[2] == board[5] && board[5] == board[8] {
-			return GameResult.wine(board[2]!)
-		}
-		
-		if board[0] != nil &&  board[0] == board[4] && board[4] == board[8] {
-			return GameResult.wine(board[0]!)
-		}
-		if board[2] != nil &&  board[2] == board[4] && board[4] == board[6] {
-			return GameResult.wine(board[2]!)
-		}
-		
-		if !board.contains(nil) {
-			return .draw
-		}
-		
-		
-		
-		return .unknown
-	}
-	
-	func toString(player1: Player, player2: Player) -> String {
-		let positions = genarateBoardState(player1: player1, player2: player2)
+	func printBoard() -> String {
+		let positions = genarateBoardState()
 		var str = ""
 		var newLine = 0
 		for element in positions {
@@ -122,5 +78,57 @@ class Board {
 			}
 		}
 		return str
+	}
+	
+	private func checkGameResult(_ board: [PlayerType?]) -> GameResult {
+		if board[0] != nil &&  board[0] == board[1] && board[1] == board[2] {
+			return GameResult.win(board[0]!)
+		}
+		if board[3] != nil &&  board[3] == board[4] && board[4] == board[5] {
+			return GameResult.win(board[3]!)
+		}
+		if board[6] != nil &&  board[6] == board[7] && board[7] == board[8] {
+			return GameResult.win(board[6]!)
+		}
+		
+		if board[0] != nil &&  board[0] == board[3] && board[3] == board[6] {
+			return GameResult.win(board[0]!)
+		}
+		if board[1] != nil &&  board[1] == board[4] && board[4] == board[7] {
+			return GameResult.win(board[1]!)
+		}
+		if board[2] != nil &&  board[2] == board[5] && board[5] == board[8] {
+			return GameResult.win(board[2]!)
+		}
+		
+		if board[0] != nil &&  board[0] == board[4] && board[4] == board[8] {
+			return GameResult.win(board[0]!)
+		}
+		if board[2] != nil &&  board[2] == board[4] && board[4] == board[6] {
+			return GameResult.win(board[2]!)
+		}
+		
+		if !board.contains(nil) {
+			return .draw
+		}
+		
+		return .unknown
+	}
+	
+	
+	
+	private func genarateBoardState() -> [PlayerType?] {
+		var positions = [PlayerType?](repeating: nil, count: Constants.boardSideCount * Constants.boardSideCount)
+		setPlayerMoves(board: &positions, player: player1)
+		setPlayerMoves(board: &positions, player: player2)
+		return positions
+	}
+	
+	private func setPlayerMoves(board: inout [PlayerType?], player: Player) {
+		for (index, item) in player.moves.enumerated() {
+			if item {
+				board[index] = player.type
+			}
+		}
 	}
 }
--- Part 12/Exc_1/Exc_1/main.swift
+++ Part 12/Exc_1/Exc_1/main.swift
@@ -5,26 +5,24 @@ var noughtsCount = 0
 var crossesCount = 0
 var drawCount = 0
 
-func qwe(board: Board, player1: Player, player2: Player){
-	let player1AllowedMoves = board.getPossibleMoves(player1: player1, player2: player2)
-	for moveIndex in player1AllowedMoves {
-		player1.addMove(Move(move: moveIndex))
-		let result = board.calculateGameResult(player1: player1, player2: player2)
-		switch result {
+func calculateGamesCount(board: Board, player1: Player, player2: Player){
+	let availableMoves = board.getAvailableMoves()
+	for moveIndex in availableMoves {
+		player1.addMove(Move(position: moveIndex))
+		switch board.calculateGameResult() {
 		case .unknown:
-			qwe(board: board, player1: player2, player2: player1)
-		case .wine(let type):
+			calculateGamesCount(board: board, player1: player2, player2: player1)
+		case .win(let type):
 			type == .crosses ? (crossesCount += 1) : (noughtsCount += 1)
 		case .draw:
 			drawCount += 1
 		}
-		player1.removeMove(Move(move: moveIndex))
+		player1.removeMove(Move(position: moveIndex))
 	}
 }
 
-
-let player1 = Player(type: .crosses)
-let player2 = Player(type: .noughts)
-let board = Board()
-qwe(board: board, player1: player1, player2: player2)
+let crossesPlayer = Player(type: .crosses)
+let noughtsPlayer = Player(type: .noughts)
+let board = Board(player1: crossesPlayer, player2: noughtsPlayer)
+calculateGamesCount(board: board, player1: crossesPlayer, player2: noughtsPlayer)
 print("X: \(crossesCount), O: \(noughtsCount), #: \(drawCount) ")
